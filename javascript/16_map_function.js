34ea8b839be758bef5420e2de1adf39a1b930448
CB.ORG\mcampbell <mcampbell@collegeboard.org>
2018-04-25 13:05:11 -0400

    Added map function (similar to vector operations in R)

---
+++ javascript/16_map_function.js
@@ -0,0 +1,10 @@
+
+// WORKS LIKE R VECTORIZED OPERATIONS
+
+var x = [20,21,22,23]
+
+x > 21 // not gonna work like R
+
+x.map( i => i > 21) // but this returns an array of true /false, like `x > 21` would in R
+
+
