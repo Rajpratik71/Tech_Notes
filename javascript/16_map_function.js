9c197a7f3aa1b752cd7aebb1eec7a4c250f9b55e
CB.ORG\mcampbell <mcampbell@collegeboard.org>
2018-04-25 13:11:03 -0400

    printing map function output to console log

--- javascript/16_map_function.js
+++ javascript/16_map_function.js
@@ -5,6 +5,7 @@ var x = [20,21,22,23]
 
 x > 21 // not gonna work like R
 
-x.map( i => i > 21) // but this returns an array of true /false, like `x > 21` would in R
+y = x.map( i => i > 21) // but this returns an array of true /false, like `x > 21` would in R
 
+console.log(y);
 
